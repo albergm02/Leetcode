2d45cf60981ca1d0fbc6c6425cdf955c35d19439
albergm02 <alberto.g.m.0214@gmail.com>
2024-07-01 09:30:35 +0200

    2626

--- 30 days of Javascript/2626.arrayReduceTransformation.js
+++ 30 days of Javascript/2626.arrayReduceTransformation.js
@@ -63,5 +63,9 @@ Constraints:
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    
+    let result = init;
+    for (var i = 0; i < nums.length; i++){
+        result = fn(result, nums[i]);
+    }
+    return result;
 };
